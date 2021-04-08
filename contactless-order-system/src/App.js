78a438292ba505c8b129bc81934e076e12cdc5e5
Andre <andrefernandez250@gmail.com>
2021-04-07 23:39:48 -0400

    Initial setup

--- contactless-order-system/src/App.js
+++ contactless-order-system/src/App.js
@@ -1,11 +1,25 @@
+// Pages
+import Menu from "./pages/Menu.js";
+import Chekout from "./pages/Checkout.js";
+
+// Styles
 import "./styles/global.css";
 
+// Router
+import { Switch, Route, useLocation } from "react-router-dom";
+
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+      <Switch location={location} key={location.pathname}>
+        <Route path="/Menu" exact>
+          <Menu />
+        </Route>
+        <Route path="/Checkout" exact>
+          <Checkout />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ contactless-order-system/src/pages/Menu.js
@@ -0,0 +1,9 @@
+const Menu = () => {
+  return (
+    <div className="container">
+      <h1>Menus</h1>
+    </div>
+  );
+};
+
+export default Menu;
